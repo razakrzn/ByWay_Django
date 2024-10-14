30f77cdac3d22c847acc0b4bb4ec1d1f117ccbc1
Razaq Rzn <razaqrzn@gmail.com>
2024-10-14 12:26:49 +0530

    Initial commit with project structure and requirements

---
+++ static/javascript/swiper.js
@@ -0,0 +1,58 @@
+document
+  .querySelectorAll(".custom-swiper-button-next, .custom-swiper-button-prev")
+  .forEach((button) => {
+    button.addEventListener("keydown", (e) => {
+      if (e.key === "Enter" || e.key === " ") {
+        e.preventDefault();
+        console.log("Button clicked via keyboard");
+        button.click();
+      }
+    });
+  });
+
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 15,
+  slidesPerGroup: 3,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  navigation: {
+    nextEl: ".custom-swiper-button-next",
+    prevEl: ".custom-swiper-button-prev",
+  },
+
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+
+  keyboard: {
+    enabled: true, // Enable keyboard control
+    onlyInViewport: true,
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    360: {
+      slidesPerView: 1,
+    },
+    480: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
